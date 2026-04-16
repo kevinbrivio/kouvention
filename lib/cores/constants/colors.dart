40d7c1ff10e8aeaa17a73890e0760dafa7010451
Kevin Brivio <kenkenku6@gmail.com>
2026-04-16 16:53:59 +0700

    add: custom colors

---
+++ lib/cores/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6200EE);
+
+  static const Color backdrop = Color(0x503D3D3D);
+  static const Color transparent = Colors.transparent;
+}
