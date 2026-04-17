61f8368c997d5f0504217d51983532f9901b72da
Kevin Brivio <kenkenku6@gmail.com>
2026-04-17 16:30:24 +0700

    add: fonts

---
+++ lib/cores/constants/text_theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kouvention/cores/constants/colors.dart';
+
+class _TextTheme {
+  final headline1 = TextStyle(
+    fontSize: 42.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.bold,
+    height: 1.5,
+  );
+  final subheadline1 = TextStyle(
+    fontSize: 24.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+  );
+  final body1 = TextStyle(
+    fontSize: 16.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+  final body2 = TextStyle(
+    fontSize: 12.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+}
+
+final textTheme = _TextTheme();
