8671fdb7cc1328635f48c74c3699e59a02033c38
Kevin Brivio <kenkenku6@gmail.com>
2026-04-17 17:06:24 +0700

    add: floating widget

---
+++ lib/cores/widgets/floating_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class FloatingWidget extends StatefulWidget {
+  const FloatingWidget({super.key, required this.child});
+  final Widget child;
+
+  @override
+  State<FloatingWidget> createState() => _FloatingWidgetState();
+}
+
+class _FloatingWidgetState extends State<FloatingWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true); // goes up then down, forever
+
+    _animation = Tween<double>(
+      begin: -10, // floats 10px up
+      end: 10,    // floats 10px down
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut, // smooth, natural feel
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // always dispose!
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _animation.value), // only moves vertically
+          child: child,
+        );
+      },
+      child: widget.child,
+    );
+  }
+}
