04f3809a80748f9f6a687bb4ffa8b857e7be0458
itzKv <kenkenku6@gmail.com>
2026-04-18 15:47:55 +0700

    add: custom transparent container

---
+++ lib/cores/widgets/transparent_box.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kouvention/cores/constants/colors.dart';
+
+class TransparentBox extends StatelessWidget {
+  final Widget child;
+  final BorderRadius? radius;
+
+  const TransparentBox({
+    super.key,
+    required this.child,
+    this.radius,
+  });
+
+  @override
+  Widget build(BuildContext context) => Container(
+    decoration: BoxDecoration(
+      color: AppColors.white.withValues(alpha: 0.35),
+      borderRadius: radius ?? BorderRadius.circular(28.r),
+      border: Border.all(
+        color: AppColors.white.withValues(alpha: 0.2),
+        style: BorderStyle.solid,
+        width: 1.sp,
+      ),
+    ),
+    padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+    child: child,
+  );
+}
