fd78fa6fceb20cba1400f02ebf60413462b32790
Kevin Brivio <kenkenku6@gmail.com>
2026-04-22 10:50:38 +0700

    fix: update the sendmessage to lastMessage

--- lib/features/chat/models/chat_model.dart
+++ lib/features/chat/models/chat_model.dart
@@ -51,6 +51,8 @@ class ChatModel {
   String displayName(String currentUid) {
     if (isDirect) {
       final otherUid = otherMemberUid(currentUid);
+      print('SENDER MEMBER ID ----> $currentUid');
+      print('OTHER MEMBER ID ----> $otherUid');
       return memberInfo[otherUid]?.displayName ?? 'Unknown';
     }
     return groupName ?? 'Unnamed Group';
