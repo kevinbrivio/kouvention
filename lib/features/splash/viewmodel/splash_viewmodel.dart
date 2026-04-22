862af3b289db239f32453c232693ecdd479d6813
Kevin Brivio <kenkenku6@gmail.com>
2026-04-22 14:25:54 +0700

    update: rm unused imports

--- lib/features/splash/viewmodel/splash_viewmodel.dart
+++ lib/features/splash/viewmodel/splash_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kouvention/cores/bases/base_notifier.dart';
 import 'package:kouvention/cores/router/router_constants.dart';
