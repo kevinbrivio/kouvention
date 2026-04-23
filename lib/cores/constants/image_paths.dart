d84e4d58af1d3ec84926b35cbca9ca547ac2dda5
Kevin Brivio <kenkenku6@gmail.com>
2026-04-23 16:08:39 +0700

    add: chat wallpaper

--- lib/cores/constants/image_paths.dart
+++ lib/cores/constants/image_paths.dart
@@ -2,6 +2,7 @@ const String _imagePath = 'assets/images';
 
 class _ImagePath {
   final String logo = '$_imagePath/logo.png';
+  final String chatWallpaper = '$_imagePath/chat_wallpaper.jpg';
 }
 
 final images = _ImagePath();
