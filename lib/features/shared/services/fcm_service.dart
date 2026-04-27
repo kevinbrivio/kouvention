f854951af6158509c8ff1ae742475fc9b95d341e
Kevin Brivio <kenkenku6@gmail.com>
2026-04-27 09:38:57 +0700

    add: save token to firestore

--- lib/features/shared/services/fcm_service.dart
+++ lib/features/shared/services/fcm_service.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 
 class FcmService {
   final FirebaseMessaging _messaging = FirebaseMessaging.instance;
@@ -20,6 +23,28 @@ class FcmService {
 
     return settings.authorizationStatus == AuthorizationStatus.authorized;
   }
-  
-  
+
+  Future<void> saveToken() async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid == null) return;
+
+    final token = await _messaging.getToken();
+    if (token == null) return;
+
+    return _saveTokenToFirestore(uid, token);
+  }
+
+  Future<void> _saveTokenToFirestore(String uid, token) async {
+    await _firestore.doc('users/$uid').set({
+      'fcmTokens': {
+        'token': {
+          'device': _getDevicePlatform(),
+          'updatedAt': FieldValue.serverTimestamp(),
+        },
+      },
+    }, SetOptions(merge: true)); // => Create if doc is missing, replace if exist
+  }
+
+  String _getDevicePlatform() =>
+      defaultTargetPlatform == TargetPlatform.iOS ? 'ios' : 'android';
 }
