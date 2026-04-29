d0885f0111f9f66e7cbf205be3282d60452b9d64
Kevin Brivio <kenkenku6@gmail.com>
2026-04-29 13:57:50 +0700

    add: date formatting

---
+++ lib/cores/utils/date_time_helper.dart
@@ -0,0 +1,40 @@
+import 'package:intl/intl.dart';
+
+class DateTimeHelper {
+  static Map<String, int> weekOrder = {
+    'Mon': 0,
+    'Tue': 1,
+    'Wed': 2,
+    'Thu': 3,
+    'Fri': 4,
+    'Sat': 5,
+    'Sun': 6,
+  };
+
+  static Map<int, String> regularWeek = {
+    1: 'Mon',
+    2: 'Tue',
+    3: 'Wed',
+    4: 'Thu',
+    5: 'Fri',
+    6: 'Sat',
+    7: 'Sun',
+  };
+
+  static const Map<int, String> regularMonths = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+  };
+  static String formatDateMonthYear(DateTime date) =>
+      DateFormat('dd/MM/yyyy').format(date);
+}
