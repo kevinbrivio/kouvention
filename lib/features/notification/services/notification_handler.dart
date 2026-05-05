df5f36f37bb5c4bc01d0267213f05b89f58724c3
Kevin Brivio <kenkenku6@gmail.com>
2026-05-05 14:09:50 +0700

    add: notification receiver

--- lib/features/notification/services/notification_handler.dart
+++ lib/features/notification/services/notification_handler.dart
@@ -1,10 +1,150 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:go_router/go_router.dart';
 import 'package:kouvention/cores/router/router.dart';
+import 'package:kouvention/cores/router/router_constants.dart';
+import 'package:kouvention/features/chat/services/chat_service.dart';
 import 'package:kouvention/features/notification/viewmodel/active_chat_id_provider.dart';
+import 'package:kouvention/firebase_options.dart';
+
+@pragma('vm:entry-point')
+void onBackgroundNotificationResponse(NotificationResponse response) async {
+  // User typed reply
+  final String? replyText = response.input;
+  final String? chatId = response.payload;
+
+  if (replyText == null || chatId == null) return;
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Refresh the firebase
+  final currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser == null) return;
+
+  await currentUser.getIdToken(true);
+
+  // Inject Chat Service
+  final chatService = ChatService();
+  final chat = await chatService.getChat(chatId);
+  if (chat == null) return;
+
+  // Write to firestore
+  await chatService.sendMessage(
+    chatId: chatId,
+    senderId: currentUser.uid,
+    text: replyText,
+    memberUids: chat.members,
+  );
+  await _sendReplyFromNotification(chatId, replyText);
+}
+
+@pragma('vm:entry-point')
+Future<void> firebaseBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  final plugin = FlutterLocalNotificationsPlugin();
+
+  await plugin
+      .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin
+      >()
+      ?.createNotificationChannel(
+        AndroidNotificationChannel(
+          'chat_messages',
+          'Chat Messages',
+          description: 'Notifications for new message',
+          importance: Importance.high,
+        ),
+      );
+
+  await plugin.initialize(
+    settings: const InitializationSettings(
+      android: AndroidInitializationSettings('ic_notification'),
+    ),
+  );
+
+  final data = message.data;
+  await _showChatNotification(
+    plugin: plugin,
+    chatId: data['chatId'] ?? '',
+    senderId: data['senderId'] ?? '',
+    senderName: data['senderName'] ?? 'Someone',
+    title: data['title'] ?? '',
+    body: data['body'] ?? '',
+  );
+}
+
+Future<void> _showChatNotification({
+  required FlutterLocalNotificationsPlugin plugin,
+  required String chatId,
+  required String senderId,
+  required String senderName,
+  required String title,
+  required String body,
+}) async {
+  // Get the user creds
+  final currentUserUid = FirebaseAuth.instance.currentUser?.uid ?? 'Unknown_id';
+  final MessagingStyleInformation messageStyle = MessagingStyleInformation(
+    Person(
+      key: currentUserUid,
+      name: 'You',
+      // icon: TODO: Add sender icon
+    ),
+    messages: [
+      Message(body, DateTime.now(), Person(key: senderId, name: senderName)),
+    ],
+  );
+
+  const replyAction = AndroidNotificationAction(
+    'reply_action',
+    'Reply',
+    inputs: [AndroidNotificationActionInput(label: 'Type a message...')],
+    showsUserInterface: false,
+  );
+
+  await plugin.show(
+    id: chatId.hashCode,
+    title: title,
+    body: body,
+    payload: chatId,
+    notificationDetails: NotificationDetails(
+      android: AndroidNotificationDetails(
+        'chat_messages',
+        'Chat messages',
+        importance: Importance.high,
+        priority: Priority.high,
+        styleInformation: messageStyle,
+        actions: [replyAction],
+      ),
+    ),
+  );
+}
+
+Future<void> _sendReplyFromNotification(String chatId, String text) async {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  if (currentUser == null) return;
+
+  try {
+    await FirebaseFirestore.instance
+        .collection('chats')
+        .doc(chatId)
+        .collection('messages')
+        .add({
+          'text': text,
+          'senderId': currentUser.uid,
+          'sentAt': FieldValue.serverTimestamp(),
+          'type': text, // Reply only can send text-type
+        });
+  } catch (e) {
+    debugPrint('Sending reply text error: $e');
+  }
+}
 
 class NotificationHandler {
   final Ref _ref;
@@ -38,9 +178,11 @@ class NotificationHandler {
     // 2. setup the local notification plugin
     await _localNotifications.initialize(
       settings: const InitializationSettings(
-        android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+        android: AndroidInitializationSettings('ic_notification'),
       ),
       onDidReceiveNotificationResponse: _onNotificationTapped,
+      onDidReceiveBackgroundNotificationResponse:
+          onBackgroundNotificationResponse,
     );
 
     // 3. Listen for foreground message
@@ -69,34 +211,28 @@ class NotificationHandler {
   }
 
   void _showLocalNotification(RemoteMessage message) {
-    final notif = message.notification;
-    if (notif == null) return;
+    final data = message.data;
+    final chatId = data['chatId'] ?? '';
+    final senderName = data['senderName'] ?? 'Someone';
+    final senderId = data['senderId'] ?? '';
+    final body = data['body'] ?? '';
+    final title = data['title'] ?? 'New message';
 
-    final chatId = message.data['chatId'] ?? '';
-
-    _localNotifications.show(
-      id: chatId.hashCode,
-      title: notif.title ?? 'New message',
-      body: notif.body ?? '',
-      notificationDetails: NotificationDetails(
-        android: AndroidNotificationDetails(
-          _androidChannel.id,
-          _androidChannel.name,
-          channelDescription: _androidChannel.description,
-          importance: Importance.high,
-          priority: Priority.high,
-        ),
-      ),
-      payload: chatId,
+    _showChatNotification(
+      plugin: _localNotifications,
+      chatId: chatId,
+      senderId: senderId,
+      senderName: senderName,
+      title: title,
+      body: body,
     );
   }
 
   // Tap in a local notification (foreground)
   void _onNotificationTapped(NotificationResponse response) {
     final chatId = response.payload;
-    if (chatId != null && chatId.isNotEmpty) {
-      _navigateToChat(chatId);
-    }
+    if (chatId == null || chatId.isEmpty) return;
+    _navigateToChat(chatId);
   }
 
   // Tap a system notification (background/terminated)
@@ -108,6 +244,6 @@ class NotificationHandler {
   }
 
   void _navigateToChat(String chatId) {
-    router.push('chat/$chatId');
+    router.push('/chats/$chatId');
   }
 }
