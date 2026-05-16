3f72dc212cd1a62fe393bb7ec355992c0eb37bcc
Kevin Brivio <kenkenku6@gmail.com>
2026-05-16 14:06:16 +0700

    add: secure layer for sqlite

---
+++ lib/cores/services/db_key_manager.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+/// Manage encryption key for SQLite database.
+/// This key will be generated ONCE when running the app
+/// If the key is missing/gone, old database will be removed
+/// And user will use new database.
+class DbKeyManager {
+  static const _storage = FlutterSecureStorage();
+  static const _keyName = '_sqlite_encryption_key_';
+
+  static Future<String> getOrCreateKey() async {
+    // 1. Load the old key (if exist)
+    final existingKey = await _storage.read(key: _keyName);
+    if (existingKey != null) return existingKey;
+
+    // 2. No old key -> Generate a new one
+    final random = Random.secure();
+    final bytes = List<int>.generate(32, (_) => random.nextInt(256));
+
+    // 3. Convert base64 to String (SQLite pragma accepts string)
+    final newKey = base64Url.encode(bytes);
+
+    // 4. Save in storage
+    await _storage.write(key: _keyName, value: newKey);
+
+    return newKey;
+  }
+
+  
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import firebase_crashlytics
 import firebase_messaging
 import firebase_storage
 import flutter_local_notifications
+import flutter_secure_storage_darwin
 import google_sign_in_ios
 import shared_preferences_foundation
 import video_player_avfoundation
@@ -30,6 +31,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
