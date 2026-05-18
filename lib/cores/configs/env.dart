32028592a98149f7c3a61dbe991063581a01910a
Kevin Brivio <kenkenku6@gmail.com>
2026-05-18 09:22:21 +0700

    add: new config

--- lib/cores/configs/env.dart
+++ lib/cores/configs/env.dart
@@ -8,6 +8,12 @@ abstract class EnvStaging {
   static const bool showBanner = _EnvStaging.showBanner;
   @EnviedField(varName: 'GOOGLE_SERVER_CLIENT_ID')
   static const String googleServerClientId = _EnvStaging.googleServerClientId;
+
+  // === CLOUDINARY ===
+  @EnviedField(varName: 'CLOUDINARY_CLOUD_NAME')
+  static const String cloudinaryCloudName = _EnvStaging.cloudinaryCloudName;
+  @EnviedField(varName: 'CLOUDINARY_UPLOAD_PRESET')
+  static const String cloudinaryUploadPreset = _EnvStaging.cloudinaryUploadPreset;
 }
 
 @Envied(path: '.env.prod')
@@ -16,4 +22,10 @@ abstract class EnvProd {
   static const bool showBanner = _EnvProd.showBanner;
   @EnviedField(varName: 'GOOGLE_SERVER_CLIENT_ID')
   static const String googleServerClientId = _EnvProd.googleServerClientId;
+
+  // === CLOUDINARY ===
+  @EnviedField(varName: 'CLOUDINARY_CLOUD_NAME')
+  static const String cloudinaryCloudName = _EnvProd.cloudinaryCloudName;
+  @EnviedField(varName: 'CLOUDINARY_UPLOAD_PRESET')
+  static const String cloudinaryUploadPreset = _EnvProd.cloudinaryUploadPreset;
 }
