176df516c22831ad53557c1aaebf81dcb5289403
Kevin Brivio <kenkenku6@gmail.com>
2026-05-21 09:49:43 +0700

    update: mediaUrls

--- lib/features/chat/models/message_model.dart
+++ lib/features/chat/models/message_model.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kouvention/features/chat/models/message_type.dart';
 import 'package:kouvention/features/chat/models/reply_to_model.dart';
 
 class MessageModel {
@@ -6,33 +7,47 @@ class MessageModel {
   final String senderId;
   final String senderName;
   final String text;
-  final String type; // "text" for now, "image" | "file" | "video" later
+  final MessageType type;
   final DateTime sentAt;
   final ReplyToModel? replyTo;
   final bool isDeleted;
   final List<String> deletedFor;
 
+  final String? mimeType;
+  final int? mediaDuration;
+
   // FUTURE CASES
-  final String? mediaUrl;
+  final List<String>? mediaUrls;
   final String? fileName;
   final int? fileSizeBytes;
+
+  bool get hasMedia => mediaUrls != null;
+  bool get isImage => type == MessageType.image;
+  bool get isVideo => type == MessageType.video;
+  bool get isAudio => type == MessageType.audio;
+  bool get isFile  => type == MessageType.file;
+
+  List<String> get allMediaUrls {
+    if (mediaUrls != null && mediaUrls!.isNotEmpty) return mediaUrls!;
+    return [];
+  }
 
   const MessageModel({
     required this.id,
     required this.senderId,
     required this.senderName,
     required this.text,
-    this.type = 'text',
+    this.type = MessageType.text,
     required this.sentAt,
     this.replyTo,
-    this.mediaUrl,
+    this.mimeType,
+    this.mediaDuration,
+    this.mediaUrls,
     this.fileName,
     this.fileSizeBytes,
     this.isDeleted = false,
     this.deletedFor = const [],
   });
-
-  bool get hasMedia => mediaUrl != null;
 
   factory MessageModel.fromMap(String docId, Map<String, dynamic> data) =>
       MessageModel(
@@ -40,30 +55,44 @@ class MessageModel {
         senderId: data['senderId'] as String,
         senderName: data['senderName'] as String,
         text: data['text'] as String? ?? '',
-        type: data['type'] as String? ?? 'text',
+        type: MessageType.fromString(data['type'] as String? ?? 'text'),
         sentAt: (data['sentAt'] as Timestamp?)?.toDate() ?? DateTime.now(),
         replyTo: (data['replyTo'] != null)
             ? ReplyToModel.fromMap(data['replyTo'])
             : null,
-        mediaUrl: data['mediaUrl'] as String?,
+        mediaUrls: data['mediaUrl'] as List<String>?,
         fileName: data['fileName'] as String?,
+        mimeType: data['mimeType'] as String?,
+        mediaDuration: (data['mediaDuration'] as num?)?.toInt(),
         fileSizeBytes: (data['fileSizeBytes'] as num?)?.toInt(),
         isDeleted: data['isDeleted'] ?? false,
-        deletedFor: List<String>.from(data['deletedFor'] ?? [])
+        deletedFor: List<String>.from(data['deletedFor'] ?? []),
       );
 
   static Map<String, dynamic> toNewMessageMap({
     required String senderId,
     required String senderName,
     required String text,
+    MessageType type = MessageType.text,
     ReplyToModel? replyTo,
+    // Media files
+    List<String>? mediaUrls,
+    String? fileName,
+    int? fileSizeBytes,
+    String? mimeType,
+    int? mediaDuration,
   }) {
     return {
       'senderId': senderId,
       'senderName': senderName,
       'text': text,
-      'type': 'text',
+      'type': type.name,
       if (replyTo != null) 'replyTo': replyTo.toMap(),
+      if (mediaUrls != null) 'mediaUrls': mediaUrls,
+      if (fileName != null) 'fileName': fileName,
+      if (fileSizeBytes != null) 'fileSizeBytes': fileSizeBytes,
+      if (mimeType != null) 'mimeType': mimeType,
+      if (mediaDuration != null) 'mediaDuration': mediaDuration,
       'sentAt': FieldValue.serverTimestamp(),
     };
   }
@@ -72,6 +101,7 @@ class MessageModel {
     required String senderId,
     required String senderName,
     required String text,
+    MessageType type = MessageType.text,
     ReplyToModel? replyTo,
   }) {
     return {
@@ -81,7 +111,7 @@ class MessageModel {
         'senderName': senderName,
         if (replyTo != null) 'replyTo': replyTo.toMap(),
         'sentAt': FieldValue.serverTimestamp(),
-        'type': 'text',
+        'type': type.name,
       },
     };
   }
