60ef5ebe621b700a3095617612dc10bce409b991
Kevin Brivio <kenkenku6@gmail.com>
2026-05-21 10:16:06 +0700

    fix: material dart

--- lib/features/chat/viewmodel/chat_room_viewmodel.dart
+++ lib/features/chat/viewmodel/chat_room_viewmodel.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:drift/drift.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
